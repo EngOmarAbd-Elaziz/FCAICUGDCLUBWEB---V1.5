cd0c62a8e6983e3a56d80721d6901d38fcabe68c
fcaigdc <omar.abdelaziz.gamedev@gmail.com>
2026-08-18 18:47:36 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default async function HomePage() {
 
       {/* Linktree / Social Hub Banner */}
       <a
-        href="https://linktr.ee/fcaicugdclub"
+        href="https://linktr.ee/fcai_gd"
         target="_blank"
         rel="noreferrer"
         style={{ textDecoration: 'none', display: 'block' }}
